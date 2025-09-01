6bffa22cf2205608e028f4926995a18bb99db1fa
MohammadDanialNasiri <dandannasiri@gmail.com>
2025-09-01 18:00:20 +0330

    Register

--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -12,3 +15,26 @@ def user_login(request):
             login(request, user)
             return redirect('/')
     return render(request, 'account/login.html',{})
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
+
+def user_register(request):
+    context = {'errors': []}
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 != password2:
+            context['errors'].append('passwords do not match')
+            return render(request, 'account/register.html', context)
+
+        user = User.objects.create(username=username, email=email, password=password1)
+        login(request, user)
+        return redirect('/')
+    return render(request, 'account/register.html', {})
